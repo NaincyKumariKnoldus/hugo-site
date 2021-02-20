c8b09d53faa2d22f947c4fddc0a4fa3d57f7589b
druskus20 <druskus20@gmail.com>
2021-02-20 15:38:31 +0100

    header and nav

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,81 @@
+/**
+ * My dirty js code. Ideally eventually wont use jquery.
+ */
+
+var articleStart;
+function loadDOMHandler() {
+  //articleStart = ($('#hero').height() + $('#nav').height());
+  articleStart = ($('#header').height() + 100);
+
+  // Checks for the theme stored in the browser
+  const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", loadDOMHandler);
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar 
+    source https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
+*/
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = throttle(100, function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
+});
+
+
+
+/* Add shadow 
+https://stackoverflow.com/questions/40967682/navbar-changing-to-add-shadow-on-scroll
+*/
+
+function throttle(limit, callback) {
+  var waiting = false;                      // Initially, we're not waiting
+  return function () {                      // We return a throttled function
+    if (!waiting) {                       // If we're not waiting
+      callback.apply(this, arguments);  // Execute users function
+      waiting = true;                   // Prevent future invocations
+      setTimeout(function () {          // After a period of time
+        waiting = false;              // And allow future invocations
+      }, limit);
+    }
+  }
+}
+
+// Only run on index.html
+/*if (window.location.pathname === '/') {*/
+$(window).scroll(
+  throttle(50,
+    function () {
+    // let height = ($('#hero').height() + $('#nav').height());
+      if ($(window).scrollTop() >= articleStart) {
+        $('header').addClass('header-shadow');
+      }
+      else {
+        $('header').removeClass('header-shadow');
+      }
+}));
+
+
+
+
+
+
+
+
+
+
 function themeSwitch() {
   let theme = document.documentElement.getAttribute('data-theme');
   if (theme == null || theme == 'light') {
@@ -24,5 +102,8 @@ function navToogle() {
       nav.style.opacity = "0";  
     }
   }
+
+  
+
 
   
